5b2847b9e4e5e315c7a3830b447a1449fdd90cc3
PV02 <94601649+PV02@users.noreply.github.com>
2023-10-27 03:05:06 +0200

    Add files via upload

---
+++ dockerfile.py
@@ -0,0 +1,23 @@
+# Utiliser une image Python officielle comme image parente
+FROM python:3.8-slim
+
+# Définir le répertoire de travail dans le conteneur
+WORKDIR /app
+
+# Copier les fichiers nécessaires dans le conteneur
+COPY requirements.txt ./
+COPY keras_model.h5 ./
+COPY infomodel.py ./
+COPY application.py ./
+COPY static ./static/
+COPY templates ./templates/
+COPY labels.txt ./
+
+# Installer les dépendances
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Exposer le port sur lequel l'application s'exécutera
+EXPOSE 5000
+
+# Commande pour exécuter l'application
+CMD ["python", "./application.py"]
